2da344cda48b42b669fcc617a7a15940888e8882
SeekerGAO <1532584997@qq.com>
2020-06-29 18:26:20 +0800

    feat:控制器-资源

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Req } from '@nestjs/common';
+import { Controller, Get, Post,  Req} from '@nestjs/common';
 import { Request } from 'express';
 
 @Controller('cats')
@@ -14,5 +14,11 @@ export class CatsController {
 		return {
 			name: 'Tom',
 		}
+	}
+
+	@Post('add')
+	// 新增
+	create(): string {
+		return 'Add info success!';
 	}
 }
